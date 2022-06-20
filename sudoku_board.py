a3e4530d8801c50f8e5ba1ba30fbd3ce4618bbcd
Lucas Kim <lucas.es.kim9@gmail.com>
2022-06-20 13:35:16 -0700

    Displaying board changes

--- sudoku_board.py
+++ sudoku_board.py
@@ -1,4 +1,4 @@
-init_board = [
+my_board = [
     [7,8,0,4,0,0,1,2,0],
     [6,0,0,0,7,5,0,0,9],
     [0,0,0,6,0,1,0,7,8],
@@ -10,38 +10,71 @@ init_board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
-test_board = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8]
-]
+
 def print_board(board):
     for i in range(len(board)):
         if i % 3 == 0 and i != 0:
             print("- - - - - - - - - - - - ")
 
-        for j in range(len(board)):
+        for j in range(len(board[0])):
             if j % 3 == 0 and j != 0:
                 print(" | ", end="")
-            # Skip a line
-            if j == 8:
+
+            if j == 8:  # Skip a line
                 print(board[i][j])
             else:
                 print(str(board[i][j]) + "", end=" ")
 
-def test_print(board):
+
+def find_empty(board):
     for i in range(len(board)):
-        if i == 1 or i == 2:
-            print("------------")
-        for j in range(len(board)):
-            if j == 1 or j == 2:
-                print(" | ", end="")
-            if j == 2:
-                print(board[i][j])
-                print("\n")
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i,j)  # Row, Column
+    return False
+
+
+def is_valid(board, num, pos):
+    # Row
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Column
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # 3x3 Box
+    x = pos[1] // 3
+    y = pos[0] // 3
+
+    for i in range(y*3, y*3 + 3):
+        for j in range(x*3, x*3 + 3):
+            if board[i][j] == num and (i,j) != pos:
+                return False
+    return True
+
+
+def solve(board):
+    print(board)
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+    for i in range(1, 10):
+        if is_valid(board, i, (row, col)):
+            board[row][col] = i
+
+            if solve(board):
+                return True
             else:
-                print(str(board[i][j]) + "", end=" ")
+                board[row][col] = 0
+    return False
 
 
-test_print(test_board)
-#print_board(init_board)
+print_board(my_board)
+solve(my_board)
+print("*********************************")
+print_board(my_board)
